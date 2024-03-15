af1f9cca9fb13b55495e52992984419a3d19fa9c
Tu Quynh <aqv9882@nyu.edu>
2024-03-15 18:53:42 +0800

    Completed Healthbars

---
+++ Assets/Health/MonsterHealth/MonsterHealthBar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonsterHealthBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+       fill.color= gradient.Evaluate(1f);
+    }
+    
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+}
